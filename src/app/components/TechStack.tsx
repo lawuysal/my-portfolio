e199ddef2d5d2043b9aebc289c30336b88837ecb
Oğuzhan Uysal <law-tekken3@hotmail.com>
2024-11-10 17:01:13 +0300

    Added links to tech stack images.

--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -1,28 +1,5 @@
 import TechStackImage from "./TechStackImage";
 
-const images = [
-  { src: "/tech-stack/logo-javascript.png", alt: "logo of javascript" },
-  { src: "/tech-stack/logo-typescript.png", alt: "logo of typescript" },
-  { src: "/tech-stack/logo-csharp.png", alt: "logo of c sharp" },
-  { src: "/tech-stack/logo-react.webp", alt: "logo of react" },
-  { src: "/tech-stack/logo-nodejs.png", alt: "logo of nodejs" },
-  {
-    src: "/tech-stack/logo-aspnetcore.png",
-    alt: "logo of asp .net core",
-    bg: true,
-  },
-  { src: "/tech-stack/logo-expressjs.png", alt: "logo of expressjs" },
-  { src: "/tech-stack/logo-tanstackquery.png", alt: "logo of tanstack query" },
-  {
-    src: "/tech-stack/logo-reactrouter.png",
-    alt: "logo of react router",
-    bg: true,
-  },
-  { src: "/tech-stack/logo-redux.png", alt: "logo of redux" },
-  { src: "/tech-stack/logo-prisma.png", alt: "logo of prisma ORM", bg: true },
-  { src: "/tech-stack/logo-tailwind.png", alt: "logo of tailwind css" },
-];
-
 export default function TechStack() {
   return (
     <section
@@ -41,3 +18,69 @@ export default function TechStack() {
     </section>
   );
 }
+
+const images = [
+  {
+    src: "/tech-stack/logo-javascript.png",
+    alt: "logo of javascript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    src: "/tech-stack/logo-typescript.png",
+    alt: "logo of typescript",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    src: "/tech-stack/logo-csharp.png",
+    alt: "logo of c sharp",
+    link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
+  },
+  {
+    src: "/tech-stack/logo-react.webp",
+    alt: "logo of react",
+    link: "https://react.dev/",
+  },
+  {
+    src: "/tech-stack/logo-nodejs.png",
+    alt: "logo of nodejs",
+    link: "https://nodejs.org/en",
+  },
+  {
+    src: "/tech-stack/logo-aspnetcore.png",
+    alt: "logo of asp .net core",
+    link: "https://learn.microsoft.com/en-us/aspnet/core/?view=aspnetcore-8.0",
+    bg: true,
+  },
+  {
+    src: "/tech-stack/logo-expressjs.png",
+    alt: "logo of expressjs",
+    link: "https://expressjs.com/",
+  },
+  {
+    src: "/tech-stack/logo-tanstackquery.png",
+    alt: "logo of tanstack query",
+    link: "https://tanstack.com/query/latest",
+  },
+  {
+    src: "/tech-stack/logo-reactrouter.png",
+    alt: "logo of react router",
+    link: "https://reactrouter.com/en/main",
+    bg: true,
+  },
+  {
+    src: "/tech-stack/logo-redux.png",
+    alt: "logo of redux",
+    link: "https://redux-toolkit.js.org/",
+  },
+  {
+    src: "/tech-stack/logo-prisma.png",
+    alt: "logo of prisma ORM",
+    link: "https://www.prisma.io/",
+    bg: true,
+  },
+  {
+    src: "/tech-stack/logo-tailwind.png",
+    alt: "logo of tailwind css",
+    link: "https://tailwindcss.com/",
+  },
+];
--- src/app/components/TechStackImage.tsx
+++ src/app/components/TechStackImage.tsx
@@ -3,19 +3,27 @@ import Image from "next/image";
 type TechStackImageProps = {
   src: string;
   alt: string;
+  link: string;
   bg?: boolean;
 };
 
-export default function TechStackImage({ src, alt, bg }: TechStackImageProps) {
+export default function TechStackImage({
+  src,
+  alt,
+  bg,
+  link,
+}: TechStackImageProps) {
   return (
-    <Image
-      className={`self-center ${
-        bg ? "bg-white p-3 rounded-lg" : ""
-      } size-[60px] md:size-[90px] lg:size-[120px] object-scale-down`}
-      src={src}
-      alt={alt}
-      width={120}
-      height={120}
-    />
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <Image
+        className={`self-center ${
+          bg ? "bg-white p-3 rounded-lg" : ""
+        } size-[60px] md:size-[90px] lg:size-[120px] object-scale-down`}
+        src={src}
+        alt={alt}
+        width={120}
+        height={120}
+      />
+    </a>
   );
 }
